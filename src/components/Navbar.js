99bf614adb0b116b6412860dead16655ba6ad67a
Akshat <akshatsharma0610@gmail.com>
2023-04-23 19:22:52 +0530

    Commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,31 @@
+import { useState } from "react";
+import '../style.css'
 function Navbar(){
+    const [darkmode,setdarkmode]=useState(false);
+    function fun(){
+        if(darkmode===false)
+        {
+            document.body.classList.toggle('dark');
+            document.querySelector('.logo').classList.toggle('dark');
+            setdarkmode(true)
+        }
+        else{
+            document.body.classList.remove('dark');
+            document.querySelector('.logo').classList.remove('dark');
+            setdarkmode(false)
+        }
+        console.log(darkmode)
+    }
     return(
-        <div className='nav'>
-            Expense Tracker
-        </div>
+        <header>
+            <h1><i className="fa solid fa-coins logo"></i> Expense Tracker</h1>
+            <input type="checkbox" className="checkbox" id="checkbox"/>
+            <label for="checkbox" className="label">
+                <i className="fas fa-moon"></i>
+                <i className='fas fa-sun'></i>
+                <div className='ball' onClick={fun}></div>
+            </label>
+        </header>
     )
 }
 export default Navbar;
