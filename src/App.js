e2392da955032a5723b980ee13840ae807ed64d4
Akshat <akshatsharma0610@gmail.com>
2023-04-24 18:15:52 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import Container from 'react-bootstrap/Container';
 import Navbar from "./components/Navbar";
+
 
 function App() {
   return (
-    <>
+    <Container>
       <Navbar></Navbar>
-    </>
+    </Container>
   );
 }
 
